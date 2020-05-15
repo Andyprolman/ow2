b89c503201fcc732dd8e683d4d4668fe69bddf05
Andrew Prolman <aprolman21@gmail.com>
2020-05-15 12:34:52 -0700

    commit test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>
-    </div>
-  );
+class App extends Component {
+		constructor(props){
+			super(props)
+			this.state = {}
+
+			this.blueButton = this.blueButton.bind(this);
+		}
+
+		blueButton(){
+			alert('You clicked the blue button!')
+		}
+
+	render(){
+		return (
+			<div className="container App">
+					<h1>Welcome to Our World Too!  oskdlfl ksfjdk eo eo g refgh pe fdf dlsf ds jfoj eof oer jdsaj doa sjl bnd adkh</h1>
+					<button type="button" class="btn btn-primary" onClick={this.blueButton}>Primary</button>
+
+			</div>
+		);
+	}
 }
 
 export default App;
